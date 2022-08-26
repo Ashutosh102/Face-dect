2864f1e0c21254fe40367c63832bf08dc105a00a
Ashutosh Mohanty <ashutoshmohanty3815@gmail.com>
2022-08-26 23:25:38 +0530

    Initial Commit

---
+++ build/precache-manifest.c7bdeb6c6bd0d8a9e7c48299916f6807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40ff3508274c5ae5e02ea9973a95527",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2fa0e620bfae785c20d",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "87820864ef05188e511b",
+    "url": "/static/js/2.b94331ea.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/static/js/2.b94331ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2fa0e620bfae785c20d",
+    "url": "/static/js/main.74deef71.chunk.js"
+  },
+  {
+    "revision": "9030c7668e656f2f612a",
+    "url": "/static/js/runtime-main.6c5be910.js"
+  }
+]);
